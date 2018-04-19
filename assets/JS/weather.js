f5c07be04ee49f1084293609a0a069fb320dd7de
Richard Kim <rk.sungjin@gmail.com>
2018-04-18 22:46:06 -0700

    updated weather

--- assets/JS/weather.js
+++ assets/JS/weather.js
@@ -1,5 +1,4 @@
-//Weather Undergroun API : f6b871c148f6c510
-//Open Weather Map API : f11f4afd34871da9528775f1a3c40f4f
+
 var cityID;
 var countryID;
 var APIkey;
@@ -8,7 +7,6 @@ var queryURL;
 function clear() {
     $("#display-div").empty();
 }
-
 
 $("#clear-button").on("click", function(event) {
 
@@ -22,8 +20,6 @@ $(document).ready(function() {
   if(localStorage.query !== 'undefined') {
     getWeatherUpdates(localStorage.query);
   }
-
-
 
   $("#click-button").on("click", function() {
     cityID = $("#city").val().trim();
@@ -40,11 +36,6 @@ $(document).ready(function() {
 
       getWeatherUpdates(queryURL);
 
-          //weatherData.zipcode = localStorage.getItem("addressZip");
-          //weatherData.city = localStorage.getItem("addressCity");
-          //console.log(weatherData.city);
-          //console.log(weatherData.zipcode);
-
    });
    function getWeatherUpdates(query) {
 
@@ -58,60 +49,35 @@ $(document).ready(function() {
          console.log(response);
         
          $(".city").html("<h1>" + response.location.city + " Weather Details</h1>");
-         //$(".country").text("Country: " + response.location.country);
-         //$(".weather").text("Current Weather Details: " + response.location.wuiurl).attr("href", response.location.wuiurl);
-
-         //$(".weather").attr("src", response.location.wuiurl).text("Current Weather Details: " + response.location.wuiurl);
-
+        //  $(".country").text("Country: " + response.location.country);
+        //  $(".weather").text("Current Weather Details: " + response.location.wuiurl).attr("href", response.location.wuiurl);
          var forecast = response.forecast.simpleforecast.forecastday;
 
-
-        //  var dateTime = response.forecast.simpleforecast.forecastday.date.pretty;
-        //  var dateTime = moment();
-        //  console.log("CURRENT TIME: " + moment(dateTime).format("hh:mm"));
 
          var weatherRow = $("<div class='row'>");
 
          for(i = 0; i < 5; i++){
 
-
-        //var animalImage = $("<img>");
-        //animalImage.attr("src", results[i].images.fixed_height.url);
-
             var weatherDisplay = $("<div>");
-            //var weatherDisplay = $("<h1>");
-            //weatherDisplay.text(forecast[i].date.weekday_short);
-            //var weatherIcon = $("<img>");
-            //weatherIcon.attr("src", forecast[i].icon_url);
-            //var weatherLocation = $("<h3>");
-            //weatherLocation.text(response.current_observation.display_location.full);
-          
-            //af//weatherDisplay.addClass()
-
-            //af//var weatherImage = $('<img>');
-            //af//weatherImage.attr('src', forecast[i].icon_url);
 
             
-             
-             weatherDisplay.html("<h1>" + forecast[i].date.weekday_short + "</h1><br><img src='" + forecast[i].icon_url + "'><br><p>Location: " + "<br>" + response.current_observation.display_location.full + "<p>Date: " + "<br>" + forecast[i].date.pretty + "<p>Conditions: "+ "<br>" + forecast[i].conditions + "     " + "<br>" + "<br><span id='highTemp'>"+forecast[i].high.fahrenheit+"</span> | <span id='lowTemp'>"+forecast[i].low.fahrenheit)+"</span>";
+        //  var dateTime = response.forecast.simpleforecast.forecastday.date.pretty;
+        //  var dateTime = moment();
+        //  console.log("CURRENT TIME: " + moment(dateTime).format("hh:mm"));
+             weatherDisplay.html("<h1>" + forecast[i].date.weekday_short + "</h1><br><img src='" + forecast[i].icon_url + "'><br><p>Location: " + "<br>" + response.location.country_name + 
+             "<p>Date: " + "<br>" + forecast[i].date.month + "/" + forecast[i].date.day + "/" + forecast[i].date.year + 
+             "<p>Conditions: "+ "<br>" + forecast[i].conditions + "     " + "<br>" + "<br><span id='highTemp'>"+forecast[i].high.fahrenheit+"</span> | <span id='lowTemp'>"+forecast[i].low.fahrenheit)+"</span>";
              weatherRow.append(weatherDisplay);
-             //weatherRow.append(weatherIcon);
-             //weatherRow.append(weatherLocation);
 
          }
 
          $("#display-div").append(weatherRow);
 
-
-         //save to local storage (persistence)
          localStorage.clear();
          localStorage.setItem("query", query);
 
      });
    }
-
-   //$("#clear-all").on("click", clear);
-
           //if(weatherData.zipcode === "" && weatherData.city === ""){
               //console.log("zip code or city was not provided");
               
